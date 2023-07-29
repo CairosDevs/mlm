81382e88418244613072b795dd1fdaa2d88bc32a
Xavier <xhnl21@gmail.com>
2023-07-28 20:35:29 -0400

    se validaron loa campos, para que no genere error si esta null DSCAIDEV-64

--- resources/views/profile/partials/update-profile-asign-form.blade.php
+++ resources/views/profile/partials/update-profile-asign-form.blade.php
@@ -18,32 +18,32 @@
         @method('patch')
         <div>
             <label for="dni" class="form-label">DNI</label>
-            <x-text-input id="dni" name="dni" type="text" class="mt-1 block w-full" :value="old('dni', $asignProfile->dni)" required autofocus autocomplete="Name" />
+            <x-text-input id="dni" name="dni" type="text" class="mt-1 block w-full" :value="old('dni', $asignProfile->dni ?? '')" required autofocus autocomplete="Name" />
             <x-input-error class="mt-2" :messages="$errors->get('dni')" />
         </div>
         <div>
             <label for="country" class="form-label">Country</label>
-            <x-text-input id="country" name="country" type="text" class="mt-1 block w-full" :value="old('country', $asignProfile->country)"  required autocomplete="Last Name" />
+            <x-text-input id="country" name="country" type="text" class="mt-1 block w-full" :value="old('country', $asignProfile->country ?? '')"  required autocomplete="Last Name" />
             <x-input-error class="mt-2" :messages="$errors->get('country')" />
         </div>
         <div>
             <label for="placeBirth" class="form-label">Place of Birth</label>
-            <x-text-input id="placeBirth" name="placeBirth" type="text" class="mt-1 block w-full" :value="old('placeBirth', $asignProfile->placeBirth)" required autocomplete="Phone" />
+            <x-text-input id="placeBirth" name="placeBirth" type="text" class="mt-1 block w-full" :value="old('placeBirth', $asignProfile->placeBirth ?? '')" required autocomplete="Phone" />
             <x-input-error :messages="$errors->get('placeBirth')" class="mt-2" />
         </div>
         <div>
             <label for="birthdate" class="form-label">Birthdate</label>
-            <x-text-input id="birthdate" name="birthdate" type="text" class="mt-1 block w-full" :value="old('birthdate', $asignProfile->birthdate)" require autocomplete="demo" />
+            <x-text-input id="birthdate" name="birthdate" type="text" class="mt-1 block w-full" :value="old('birthdate', $asignProfile->birthdate ?? '')" require autocomplete="demo" />
             <x-input-error class="mt-2" :messages="$errors->get('birthdate')" />
         </div>
         <div>
         <label for="birthdate" class="form-label">Address</label>
-            <x-text-input id="address" name="address" type="text" class="mt-1 block w-full" :value="old('address', $asignProfile->address)" require autocomplete="demo" />
+            <x-text-input id="address" name="address" type="text" class="mt-1 block w-full" :value="old('address', $asignProfile->address ?? '')" require autocomplete="demo" />
             <x-input-error class="mt-2" :messages="$errors->get('address')" />
         </div>
         <div>
-        <label for="birthdate" class="form-label">Postal Code</label>
-            <x-text-input id="PostalCode" name="PostalCode" type="text" class="mt-1 block w-full" :value="old('PostalCode', $asignProfile->PostalCode)" require autocomplete="demo" />
+            <label for="birthdate" class="form-label">Postal Code</label>
+            <x-text-input id="PostalCode" name="PostalCode" type="text" class="mt-1 block w-full" :value="old('PostalCode', $asignProfile->PostalCode ?? '')" require autocomplete="demo" />
             <x-input-error class="mt-2" :messages="$errors->get('PostalCode')" />
         </div>
         <div>

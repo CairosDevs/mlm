56d7b3f0774a44f1b5dca8397f8f8055769e0d71
heriberto <sosaheriberto2001@gmail.com>
2023-08-16 18:38:14 -0400

    Agregar seeder para perfil, pin y membresia

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -34,10 +34,45 @@ class UserSeeder extends Seeder
                 'confirmed' => true,
                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
                 'remember_token' => Str::random(10),
+                'membership' => true
                 ]
             );
             $user->assignRole('System');
 
+            \App\Models\AsignProfile::create(
+                [
+                'user_id' => 1,
+                'dni' => 'documents/dni/RmOrPpVk.png',
+                'country' => '',
+                'placeBirth' => '',
+                'birthdate' => now(),
+                'address' => '',
+                'PostalCode' => '',
+                'digitalContract' => 'documents/contract/EiLOCPyK.pdf',
+                'status' => true
+                ]
+            );
+
+            \App\Models\AsingPin::create(
+                [
+                'user_id' => 1,
+                'pin' => '14975f78-04b1-4927-bc44-bdc134da8f8e	',
+                'status' => 0,
+                ]
+            );
+
+            \App\Models\OrderPayment::create(
+                [
+                'payment_id' => '25392f29',
+                'external_payment_id' => '6209282706',
+                'user_id' => 1,
+                'amount' => 20,
+                'type' => 'membership',
+                'status' => 'paid',
+                ]
+            );
+
+            //create admin user
             $user = \App\Models\User::create(
                 [
                 'name' => 'Admin',
@@ -48,10 +83,45 @@ class UserSeeder extends Seeder
                 'confirmed' => true,
                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
                 'remember_token' => Str::random(10),
+                'membership' => true
                 ]
             );
             $user->assignRole('Admin');
 
+            \App\Models\AsignProfile::create(
+                [
+                'user_id' => 2,
+                'dni' => 'documents/dni/RmOrPpVk.png',
+                'country' => '',
+                'placeBirth' => '',
+                'birthdate' => now(),
+                'address' => '',
+                'PostalCode' => '',
+                'digitalContract' => 'documents/contract/EiLOCPyK.pdf',
+                'status' => true
+                ]
+            );
+
+            \App\Models\AsingPin::create(
+                [
+                'user_id' => 2,
+                'pin' => '14975f78-04b1-4927-bc44-bdc134da8f8e	',
+                'status' => 0,
+                ]
+            );
+
+            \App\Models\OrderPayment::create(
+                [
+                'payment_id' => '25392f29',
+                'external_payment_id' => '6209282706',
+                'user_id' => 2,
+                'amount' => 20,
+                'type' => 'membership',
+                'status' => 'paid',
+                ]
+            );
+
+            //create operator
             $user = \App\Models\User::create(
                 [
                 'name' => 'Operator',
@@ -62,10 +132,44 @@ class UserSeeder extends Seeder
                 'confirmed' => true,
                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
                 'remember_token' => Str::random(10),
+                'membership' => true
                 ]
             );
             $user->assignRole('Operator');
 
+           \App\Models\AsignProfile::create(
+                [
+                'user_id' => 3,
+                'dni' => 'documents/dni/RmOrPpVk.png',
+                'country' => '',
+                'placeBirth' => '',
+                'birthdate' => now(),
+                'address' => '',
+                'PostalCode' => '',
+                'digitalContract' => 'documents/contract/EiLOCPyK.pdf',
+                'status' => true
+                ]
+            );
+
+            \App\Models\AsingPin::create(
+                [
+                'user_id' => 3,
+                'pin' => '14975f78-04b1-4927-bc44-bdc134da8f8e	',
+                'status' => 0,
+                ]
+            );
+
+            \App\Models\OrderPayment::create(
+                [
+                'payment_id' => '25392f29',
+                'external_payment_id' => '6209282706',
+                'user_id' => 3,
+                'amount' => 20,
+                'type' => 'membership',
+                'status' => 'paid',
+                ]
+            );
+            //create customer
             $user = \App\Models\User::create(
                 [
                 'name' => 'Customer',
@@ -76,9 +180,42 @@ class UserSeeder extends Seeder
                 'confirmed' => true,
                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
                 'remember_token' => Str::random(10),
+                'membership' => true
                 ]
             );
             $user->assignRole('Customer');
+
+            \App\Models\AsignProfile::create(
+                [
+                'user_id' => 4,
+                'dni' => 'documents/dni/RmOrPpVk.png',
+                'country' => '',
+                'placeBirth' => '',
+                'birthdate' => now(),
+                'address' => '',
+                'PostalCode' => '',
+                'digitalContract' => 'documents/contract/EiLOCPyK.pdf',
+                'status' => true
+                ]
+            );
+
+            \App\Models\AsingPin::create(
+                [
+                'user_id' => 4,
+                'pin' => '14975f78-04b1-4927-bc44-bdc134da8f8e	',
+                'status' => 0,
+                ]
+            );
+            \App\Models\OrderPayment::create(
+                [
+                'payment_id' => '25392f29',
+                'external_payment_id' => '6209282706',
+                'user_id' => 4,
+                'amount' => 20,
+                'type' => 'membership',
+                'status' => 'paid',
+                ]
+            );
             
             $this->call(
             [
@@ -104,9 +241,43 @@ class UserSeeder extends Seeder
                 'email_verified_at' => now(),
                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
                 'remember_token' => Str::random(10),
+                'membership' => true
                 ]
             );
             $user->assignRole('System');
+
+            \App\Models\AsignProfile::create(
+                [
+                'user_id' => 1,
+                'dni' => 'documents/dni/RmOrPpVk.png',
+                'country' => '',
+                'placeBirth' => '',
+                'birthdate' => now(),
+                'address' => '',
+                'PostalCode' => '',
+                'digitalContract' => 'documents/contract/EiLOCPyK.pdf',
+                'status' => true
+                ]
+            );
+
+            \App\Models\AsingPin::create(
+                [
+                'user_id' => 1,
+                'pin' => '14975f78-04b1-4927-bc44-bdc134da8f8e	',
+                'status' => 0,
+                ]
+            );
+
+            \App\Models\OrderPayment::create(
+                [
+                'payment_id' => '25392f29',
+                'external_payment_id' => '6209282706',
+                'user_id' => 1,
+                'amount' => 20,
+                'type' => 'membership',
+                'status' => 'paid',
+                ]
+            );
 
             $user = \App\Models\User::create(
                 [
@@ -117,9 +288,43 @@ class UserSeeder extends Seeder
                 'email_verified_at' => now(),
                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
                 'remember_token' => Str::random(10),
+                'membership' => true
                 ]
             );
             $user->assignRole('Admin');
+
+            \App\Models\AsignProfile::create(
+                [
+                'user_id' => 2,
+                'dni' => 'documents/dni/RmOrPpVk.png',
+                'country' => '',
+                'placeBirth' => '',
+                'birthdate' => now(),
+                'address' => '',
+                'PostalCode' => '',
+                'digitalContract' => 'documents/contract/EiLOCPyK.pdf',
+                'status' => true
+                ]
+            );
+
+            \App\Models\AsingPin::create(
+                [
+                'user_id' => 2,
+                'pin' => '14975f78-04b1-4927-bc44-bdc134da8f8e	',
+                'status' => 0,
+                ]
+            );
+
+            \App\Models\OrderPayment::create(
+                [
+                'payment_id' => '25392f29',
+                'external_payment_id' => '6209282706',
+                'user_id' => 2,
+                'amount' => 20,
+                'type' => 'membership',
+                'status' => 'paid',
+                ]
+            );
 
             $user = \App\Models\User::create(
                 [
@@ -130,9 +335,43 @@ class UserSeeder extends Seeder
                 'email_verified_at' => now(),
                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
                 'remember_token' => Str::random(10),
+                'membership' => true
                 ]
             );
             $user->assignRole('Operator');
+
+            \App\Models\AsignProfile::create(
+                [
+                'user_id' => 3,
+                'dni' => 'documents/dni/RmOrPpVk.png',
+                'country' => '',
+                'placeBirth' => '',
+                'birthdate' => now(),
+                'address' => '',
+                'PostalCode' => '',
+                'digitalContract' => 'documents/contract/EiLOCPyK.pdf',
+                'status' => true
+                ]
+            );
+
+            \App\Models\AsingPin::create(
+                [
+                'user_id' => 3,
+                'pin' => '14975f78-04b1-4927-bc44-bdc134da8f8e	',
+                'status' => 0,
+                ]
+            );
+
+            \App\Models\OrderPayment::create(
+                [
+                'payment_id' => '25392f29',
+                'external_payment_id' => '6209282706',
+                'user_id' => 3,
+                'amount' => 20,
+                'type' => 'membership',
+                'status' => 'paid',
+                ]
+            );
 
             $user = \App\Models\User::create(
                 [
@@ -143,10 +382,44 @@ class UserSeeder extends Seeder
                 'email_verified_at' => now(),
                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
                 'remember_token' => Str::random(10),
+                'membership' => true
                 ]
             );
             $user->assignRole('Customer');
             
+            \App\Models\AsignProfile::create(
+                [
+                'user_id' => 4,
+                'dni' => 'documents/dni/RmOrPpVk.png',
+                'country' => '',
+                'placeBirth' => '',
+                'birthdate' => now(),
+                'address' => '',
+                'PostalCode' => '',
+                'digitalContract' => 'documents/contract/EiLOCPyK.pdf',
+                'status' => true
+                ]
+            );
+
+            \App\Models\AsingPin::create(
+                [
+                'user_id' => 4,
+                'pin' => '14975f78-04b1-4927-bc44-bdc134da8f8e	',
+                'status' => 0,
+                ]
+            );
+
+            \App\Models\OrderPayment::create(
+                [
+                'payment_id' => '25392f29',
+                'external_payment_id' => '6209282706',
+                'user_id' => 4,
+                'amount' => 20,
+                'type' => 'membership',
+                'status' => 'paid',
+                ]
+            );
+
             $this->call(
             [
                //
